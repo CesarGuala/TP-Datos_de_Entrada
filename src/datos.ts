a09ba83ac627781a2645355d5ce52c9c0ae9673b
CesarGuala <104107223+CesarGuala@users.noreply.github.com>
2022-05-04 14:56:28 -0300

    Initial commit

---
+++ src/datos.ts
@@ -0,0 +1,15 @@
+let btnEnv = document.getElementById("btnEnviar");
+
+let dato = document.getElementById("dato");
+
+let rotulo = document.getElementById("rotulo");
+rotulo.innerHTML = "Que dia fue la revolución de mayo?: ";
+btnEnv.addEventListener("click" , () => {
+// TU CODIGO VA AQUI, POR EJEMPLO:
+console.log("el dato ingresado es ",dato.value);
+//}
+});
+rotulo.innerHTML = "Que dia fue la revolución de mayo?: ";
+
+
+
